ecb01028210d9ee91b516f7191f2695445a54e2e
Trần Gia Hào <131526509+giahao1411@users.noreply.github.com>
2023-10-01 21:49:47 +0700

    Delete JavaLang directory

--- JavaLang/SourceCodes/Payment.java
+++
@@ -1,6 +0,0 @@
-//Dont modify this file
-
-interface Payment {
-    public boolean pay(double amount);
-    public double checkBalance();
-}
--- JavaLang/SourceCodes/Transfer.java
+++
@@ -1,7 +0,0 @@
-//Dont modify this file
-
-interface Transfer {
-    public static final double transferFee = 0.05;
-    public boolean transfer (double amount, Transfer to);
-    public double checkBalance();
-}
